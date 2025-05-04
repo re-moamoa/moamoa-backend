367bca26c9d141b2a709e85ce8b7c714d0d07410
Yoon Yejin <124289103+yezanee@users.noreply.github.com>
2025-05-04 16:31:44 +0900

    Revert "feat: AccessToken 재발급 API 구현"

--- src/main/java/com/hbbhbank/moamoa/global/security/info/JwtUserInfo.java
+++
@@ -1,9 +0,0 @@
-package com.hbbhbank.moamoa.global.security.info;
-
-import com.hbbhbank.moamoa.user.domain.ERole;
-
-public record JwtUserInfo(
-  Long userId,
-  ERole role
-) {
-}
--- src/main/java/com/hbbhbank/moamoa/user/projection/UserSecurityForm.java
+++
@@ -1,9 +0,0 @@
-package com.hbbhbank.moamoa.user.projection;
-
-import com.hbbhbank.moamoa.user.domain.ERole;
-
-// 인증용 projection interface
-public interface UserSecurityForm {
-  Long getId();
-  ERole getRole();
-}
